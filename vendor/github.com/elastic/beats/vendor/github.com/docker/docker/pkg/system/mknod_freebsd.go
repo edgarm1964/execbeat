e6c055354b66a8fe2426bf6f85c3989ac048dc34
Edgar Matzinger <edgar@edgar-matzinger.nl>
2019-06-26 23:30:24 +0200

    go 1.10.8 uses type int for dev in mknod_freebsd.go

--- vendor/github.com/elastic/beats/vendor/github.com/docker/docker/pkg/system/mknod_freebsd.go
+++ vendor/github.com/elastic/beats/vendor/github.com/docker/docker/pkg/system/mknod_freebsd.go
@@ -9,7 +9,7 @@ import (
 // Mknod creates a filesystem node (file, device special file or named pipe) named path
 // with attributes specified by mode and dev.
 func Mknod(path string, mode uint32, dev int) error {
-	return syscall.Mknod(path, mode, uint64(dev))
+	return syscall.Mknod(path, mode, dev)
 }
 
 // Mkdev is used to build the value of linux devices (in /dev/) which specifies major
